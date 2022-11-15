3aebf17d45371cf217dd2e9a196cb7e1276dcb25
Prashant <prashantrahul141@protonmail.com>
2022-11-15 20:51:00 +0530

    moving for different index and watch page

--- index.js
+++ index.js
@@ -5,5 +5,11 @@ const file_div = document.getElementById('file-div');
 const file_text = document.getElementById('file-text');
 const file_input = document.getElementById('file');
 const playButton = document.getElementById('play');
-connectButton.addEventListener('click', connectHandler);
-playButton.addEventListener('click', playHandler);
+const videoPlayer = document.getElementById('video-player');
+if (document.URL.includes('index')) {
+    connectButton.addEventListener('click', connectHandler);
+}
+else if (document.URL.includes('watch')) {
+    onLoadWatch();
+    playButton.addEventListener('click', playHandler);
+}
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ const file_div = document.getElementById('file-div') as HTMLDivElement;
 const file_text = document.getElementById('file-text') as HTMLParagraphElement;
 const file_input = document.getElementById('file') as HTMLInputElement;
 const playButton = document.getElementById('play') as HTMLButtonElement;
+const videoPlayer = document.getElementById('video-player') as HTMLVideoElement;
 
-connectButton.addEventListener('click', connectHandler);
-
-playButton.addEventListener('click', playHandler);
+if (document.URL.includes('index')) {
+  connectButton.addEventListener('click', connectHandler);
+} else if (document.URL.includes('watch')) {
+  onLoadWatch();
+  playButton.addEventListener('click', playHandler);
+}
