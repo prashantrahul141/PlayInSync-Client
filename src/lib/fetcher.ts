e2933bcd24028b35a3807ee04ec6ba3000b37bdf
Prashant <prashantrahul141@protonmail.com>
2022-11-16 23:04:34 +0530

    using post method and posting all data to the server when changes occur

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,3 +1,10 @@
+type currentVideoState = {
+  playState: boolean;
+  playbackTime: number;
+  playbackSpeed: number;
+};
+
+// Fetcher for connecting to the server.
 const connectHandlerFetcher = async () => {
   const clientHash = Math.random().toString(36).substring(7);
   const fetch_url = new URL(clientHash, urlInput.value);
@@ -15,6 +22,7 @@ const connectHandlerFetcher = async () => {
   return { response: { state: 'Failure' } };
 };
 
+// Fetcher for sync/play event.
 const onPlayFetcher = async () => {
   const fetch_url = new URL(CLIENTHASH, SERVERURL);
 
@@ -27,11 +35,19 @@ const onPlayFetcher = async () => {
   } catch (err) {}
 };
 
-const onPlayPauseFetcher = async () => {
-  const fetch_url = new URL(`playpause/${CLIENTHASH}`, SERVERURL);
+// Fetcher for when resumed/paused
+const onPlayPauseFetcher = async (currentVideoState_: currentVideoState) => {
+  const fetch_url = new URL(`update/${CLIENTHASH}`, SERVERURL);
 
   try {
-    let res = await fetch(fetch_url);
+    let res = await fetch(fetch_url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(currentVideoState_),
+    });
     if (res) {
       let response = await res.json();
       return response;
@@ -39,11 +55,19 @@ const onPlayPauseFetcher = async () => {
   } catch (err) {}
 };
 
-const onSeekFetcher = async (_currentTime: number) => {
-  const fetch_url = new URL(`settime/${CLIENTHASH}/${_currentTime}`, SERVERURL);
+// Fetcher for when seeked.
+const onSeekFetcher = async (currentVideoState_: currentVideoState) => {
+  const fetch_url = new URL(`update/${CLIENTHASH}`, SERVERURL);
 
   try {
-    let res = await fetch(fetch_url);
+    let res = await fetch(fetch_url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(currentVideoState_),
+    });
     if (res) {
       let response = await res.json();
       return response;
@@ -51,17 +75,39 @@ const onSeekFetcher = async (_currentTime: number) => {
   } catch (err) {}
 };
 
-const onRateChangeFetcher = async (_rate: number) => {
-  const fetch_url = new URL(
-    `setplaybackspeed/${CLIENTHASH}/${_rate}`,
-    SERVERURL
-  );
+// Fetcher for video playback speed changes
+const onRateChangeFetcher = async (currentVideoState_: currentVideoState) => {
+  const fetch_url = new URL(`update/${CLIENTHASH}`, SERVERURL);
+
+  try {
+    let res = await fetch(fetch_url, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(currentVideoState_),
+    });
+    if (res) {
+      let response = await res.json();
+      return response;
+    }
+  } catch (err) {}
+};
+
+const changeListenerFetcher = async () => {
+  const fetch_url = new URL(CLIENTHASH, SERVERURL);
 
   try {
     let res = await fetch(fetch_url);
     if (res) {
       let response = await res.json();
-      return response;
+
+      if (res.status === 200 || res.status === 304) {
+        return { changed: false, ...response };
+      } else if (res.status === 201) {
+        return { changed: true, ...response };
+      }
     }
   } catch (err) {}
 };
