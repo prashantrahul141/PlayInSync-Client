0ba66d265abf6aa20a079de305b84fb26d42d615
Prashant <prashantrahul141@protonmail.com>
2022-11-16 17:21:47 +0530

    adding fetchers for onPlayPause, onSeek, onRateChange

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -12,8 +12,56 @@ const connectHandlerFetcher = async () => {
   } catch (err) {
     console.log(err);
   }
-
   return { response: { state: 'Failure' } };
 };
 
-//https://localhost:3000/
+const onPlayFetcher = async () => {
+  const fetch_url = new URL(CLIENTHASH, SERVERURL);
+
+  try {
+    let res = await fetch(fetch_url);
+    if (res) {
+      let response = await res.json();
+      return response;
+    }
+  } catch (err) {}
+};
+
+const onPlayPauseFetcher = async () => {
+  const fetch_url = new URL(`playpause/${CLIENTHASH}`, SERVERURL);
+
+  try {
+    let res = await fetch(fetch_url);
+    if (res) {
+      let response = await res.json();
+      return response;
+    }
+  } catch (err) {}
+};
+
+const onSeekFetcher = async (_currentTime: number) => {
+  const fetch_url = new URL(`settime/${CLIENTHASH}/${_currentTime}`, SERVERURL);
+
+  try {
+    let res = await fetch(fetch_url);
+    if (res) {
+      let response = await res.json();
+      return response;
+    }
+  } catch (err) {}
+};
+
+const onRateChangeFetcher = async (_rate: number) => {
+  const fetch_url = new URL(
+    `setplaybackspeed/${CLIENTHASH}/${_rate}`,
+    SERVERURL
+  );
+
+  try {
+    let res = await fetch(fetch_url);
+    if (res) {
+      let response = await res.json();
+      return response;
+    }
+  } catch (err) {}
+};
